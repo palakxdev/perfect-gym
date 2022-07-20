0320bf59a648de477717883b5590e7f27938c007
Palak-SweetCoder <pspalak6@gmail.com>
2022-07-20 21:13:13 +0600

    sign in using google and facebook authentication done

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -1,9 +1,47 @@
 import React from 'react';
 import { FaFacebook } from 'react-icons/fa';
 import { ImGoogle3 } from "react-icons/im";
+import auth from '../../../firebase.init';
+import { useSignInWithFacebook, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import './SocialLogin.css'
+import { Link, useNavigate } from 'react-router-dom';
+import Loading from '../../Common/Loading/Loading';
 
 const SocialLogin = () => {
+    const [signInWithGoogle, googleUser, googleLoading, GoogleError] = useSignInWithGoogle(auth);
+    const [signInWithFacebook, facebookUser, facebookLoading, facebookError] = useSignInWithFacebook(auth);
+    const navigate = useNavigate();
+    let errorelement;
+
+    const navigateToHome = () => {
+        navigate('/home')
+    }
+
+    if (googleUser || facebookUser) {
+        return (
+            <div className='text-center p-4 successful-regesterer m-4'>
+                <h2 className='text-success'>Congratulation!!!</h2>
+                <h2 className='mb-5 text-success'>You're successfuly registered.</h2>
+                <Link to="/home" className='text-decoration-none' onClick={navigateToHome}>----Go to home page----</Link>
+            </div>
+        );
+    }
+
+    if (googleLoading || facebookLoading) {
+        return (
+            <div className='text-center m-5 p-5'>
+                <Loading></Loading>
+                <p>Loading...</p>
+            </div>
+        );
+    }
+
+    if (GoogleError || facebookError) {
+        errorelement = <div>
+            <p className='text-danger text-center'>Error. {GoogleError?.message}</p>
+        </div>
+    }
+
     return (
         <>
             <div className='d-flex align-items-center justify-content-center'>
@@ -12,12 +50,14 @@ const SocialLogin = () => {
                 <div className='social-line'></div>
             </div>
 
-            <button className='social-button rounded-pill text-white d-flex align-items-center justify-content-center mx-auto py-2 px-4'>
-                    <ImGoogle3 className='text-primary'></ImGoogle3>
-                    <span className='ms-2'>GOOGLE SIGN IN</span>
+            {errorelement}
+
+            <button onClick={() => signInWithGoogle()} className='social-button rounded-pill text-white d-flex align-items-center justify-content-center mx-auto py-2 px-4'>
+                <ImGoogle3 className='text-primary'></ImGoogle3>
+                <span className='ms-2'>GOOGLE SIGN IN</span>
             </button>
             <br />
-            <button className='social-button rounded-pill text-white d-flex align-items-center justify-content-center mx-auto py-2 px-4'>
+            <button onClick={() => signInWithFacebook()} className='social-button rounded-pill text-white d-flex align-items-center justify-content-center mx-auto py-2 px-4'>
                 <FaFacebook className='text-primary'></FaFacebook>
                 <span className='ms-2'>FACEBOOK SIGN IN</span>
             </button>
