7a236e11d7a2b1b4a3c91591b4a562caabf883ec
Palak-SweetCoder <pspalak6@gmail.com>
2023-01-18 18:32:36 +0600

    services section tablet responsive done

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import master from '../../images/gym-master.png';
-import './About.css'
+import './About.css';
 
 const About = () => {
     return (
         <>
-            <div className='about-container d-flex align-items-center p-4'>
-                <img width={500} src={master} alt="" />
-                <div className='ms-4'>
-                    <h2 className='mb-3'> <span className='d-block mb-1'>ABOUT ME</span> PERFECT GYM CENTER</h2>
-                    <div className='about-hr'></div>
-                    <p className='mt-3 lh-base'>Hey, I'm Brian Lee. I'm a gym trainer. Are you looking for Best Gym In US? Get All Results from Across the Web. 99% Match on Best Gym In Us. Always Facts. Privacy Friendly. Unlimited Access. Results & Answers. The Best Resources. Types: Best Results, Explore Now, New Sources, Best in Search.
-                        The Perfect Gym Center is a health, recreational, and social facility geared towards exercise, sports, and other physical activities. It may be a for-profit commercial facility or a community- or institutionally-supported center.</p>
+            <div className="about-container">
+                <div className="container d-flex align-items-center p-4">
+                    <img className="img-fluid" src={master} alt="" />
+                    <div className="ms-4">
+                        <h2 className="mb-3">
+                            {' '}
+                            <span className="d-block mb-1">ABOUT ME</span>{' '}
+                            PERFECT GYM CENTER
+                        </h2>
+                        <div className="about-hr"></div>
+                        <p className="mt-3 lh-base">
+                            Hey, I'm Brian Lee. I'm a gym trainer. Are you
+                            looking for Best Gym In US? Get All Results from
+                            Across the Web. 99% Match on Best Gym In Us. Always
+                            Facts. Privacy Friendly. Unlimited Access. Results &
+                            Answers. The Best Resources. Types: Best Results,
+                            Explore Now, New Sources, Best in Search. The
+                            Perfect Gym Center is a health, recreational, and
+                            social facility geared towards exercise, sports, and
+                            other physical activities. It may be a for-profit
+                            commercial facility or a community- or
+                            institutionally-supported center.
+                        </p>
+                    </div>
                 </div>
             </div>
         </>
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,29 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import Service from '../Service/Service';
-import './Services.css'
+import './Services.css';
 
 const Services = () => {
     const [services, setServices] = useState([]);
 
     useEffect(() => {
         fetch('services.json')
-            .then(res => res.json())
-            .then(data => setServices(data))
-    }, [])
+            .then((res) => res.json())
+            .then((data) => setServices(data));
+    }, []);
 
     return (
         <>
-            <div id='services' className='mt-5 mb-5 container'>
-                <h2 className='text-center'>MY SERVICES</h2>
+            <div id="services" className="mt-5 mb-5 container">
+                <h2 className="text-center">MY SERVICES</h2>
                 <div className="y-line mx-auto"></div>
-                    <div className='services-container d-flex justify-content-center flex-wrap mt-5'>
-                        {
-                            services.map(service => <Service
-                                service={service}
-                                key={service.id}
-                            ></Service>)
-                        }
-                    </div>
+                <div className="services container mt-5">
+                    {services.map((service) => (
+                        <Service service={service} key={service.id}></Service>
+                    ))}
+                </div>
             </div>
         </>
     );
